bacb58274a0e7936717a1e5a6cb476e8858fb8f0
gilbert00 <fkemper@mail.ru>
2020-07-19 17:12:00 +0300

    Мелочь

--- src/blackjacksolitairerunner/BlackjackSolitaireRunner.java
+++ src/blackjacksolitairerunner/BlackjackSolitaireRunner.java
@@ -6,7 +6,7 @@
 
 /**
  * @author Kemper F.M. 
- * @version 0.9.0
+ * @version 0.9.1
  */
 package blackjacksolitairerunner;
 
@@ -238,8 +238,8 @@ class CalcPlayBJ{
 //    final static int[][] IND_CALC = {{0,1,2,3,4}, {5,6,7,8,9}, {10,11,12}, {13,14,15},
 //                                     {1,6,10,13}, {2,7,11,14}, {3,8,12,15}};
 //    final static int[][] IND_BJ = {{0,5}, {4,9}};
-    int[][] indCalc;
-    int[][] indBj;
+    private int[][] indCalc;
+    private int[][] indBj;
 /**    
 * Подсчет баллов игры    
 */
